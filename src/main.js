69151e1f440bc6dd467a2cee767693d60cf06fb5
Edgar Valero <edgaralexandervb@gmail.com>
2019-08-07 15:10:17 -0430

    Eliminado Vuex como dependencia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import JQuery from "jquery";
 window.$ = window.JQuery = JQuery;
 import "popper.js";
@@ -49,6 +48,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount("#app");
