2dbfa34330942e1286d6f326ffea9abfe3797bf8
Edgar Valero <edgaralexandervb@gmail.com>
2019-11-02 23:05:48 -0400

    Agregado texto en el loading.

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -3,5 +3,10 @@
     <div class="d-flex justify-content-center">
       <font-awesome-icon class="icon-spin" icon="circle-notch" spin />
     </div>
+    <div class="d-flex justify-content-center mt-1 text-center">
+      <p class="h5 ml-3">
+        Cargando contenido...
+      </p>
+    </div>
   </div>
 </template>
